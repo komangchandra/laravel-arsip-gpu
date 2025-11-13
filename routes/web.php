c6cc61d98c0db314acf5fe65cdf7b6437b611528
Komang Chandra Winata <komangchandraaa1@gmail.com>
2025-11-13 08:11:36 +0700

    refactor(document): edit routing resource

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DocumentController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -17,6 +18,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Document Routes
+    Route::resource('documents', DocumentController::class);
 });
 
 Route::middleware(['auth', 'role:super-admin'])
